1042e71a366786115d3a448f4d33e4300fc27cb6
Raisa Noshin <srn2vb@virginia.edu>
2018-07-31 06:54:57 -0400

    Added in the xml script needed to access the Average Degree Filter from Rosetta to calculate the contact number, as well as edited the overall enhancement script so that it can now calculate the distance to active site and contact number for a single case rather than in batches.

--- apps/ProteinDesign/enzyme_stabilization/enhance_enzyme_solubility.py
+++ apps/ProteinDesign/enzyme_stabilization/enhance_enzyme_solubility.py
@@ -73,9 +73,83 @@ def enhance_enzyme_solubility():
 
 	#Extracting the ligand(s) from either the input pdb file or the provided coordinates
 
-	if options.lig:
-		all_ligands = options.record_type.split()
+	with open(options.pdb) as readfile:
+		lines = readfile.readlines()
+		first_atom = 0
+		done = False
+		for line in lines:
+			first_atom += 1
+			if done:
+				break
+			items = line.split()
+			for key,value in amino_acids.iteritems():
+				if ("ATOM" in items) & (value in items):
+					done = True
+		chain = lines[first_atom-2].split()[4]
 
+	if options.lig:
+		is_selected = False
+		print "Extracting ligand coordinates..."
+		all_ligands = options.record_type.split()
+		with open(options.pdb) as readfile:
+			lines = readfile.readlines()
+			for line in lines: 
+				items = line.split()
+				if len(items[0]) > len(ligand):
+					serial_num = items[0][len(ligand):]
+					items[0] = items[0][:(len(items[0]) - len(ligand)+1)]
+					items.insert(1,serial_num)
+				if ligand not in items:
+					continue
+				format_errors = verify_line(items)
+				for current_ligand in all_ligands:
+					if (format_errors == 0) & (items[3] == current_ligand):
+						temp = items[6:9]
+						activesite.append(temp)
+					else:
+						if ((format_errors == 1) | (format_errors == -1)) & (items[3] == current_ligand):
+							print "Unexpected line format in the following line: "
+							print line
+							print "Please reformat this line so that the XYZ coordinates of the ligand are in columns 6-8"
+						if (items[2][len(items[2])-3:] == current_ligand) & ((format_errors == 2) | (format_errors == 3)):
+							if format_errors == 2:
+								temp = items[5:8]
+							if format_errors == 3:
+								temp = items[4:7]
+							activesite.append(temp)
+						if (format_errors == 4) & (items[3] == current_ligand):
+							temp = items[5:8]
+							activesite.append(temp)
+		if len(activesite) > 0:
+			is_selected = True
+		if is_selected:
+			print "Ligand coordinates successfully selected!"
+		else:
+			print "Unable to extract ligand coordinates from the input pdb file. Please check if the provided ligand ID('s) are correct."
+			return
+	else:
+		message = False
+		with open(options.asite) as readfile:
+			for line in readfile:
+				if not message:
+					print "Opening file containing active site coordinates..."
+					message = True
+				items = line.split('\r')
+				for item in items:
+					to_append = item.split(',')
+					temp = []
+					for a in to_append:
+						m = a[a.find('"')+1:a.rfind('"')]
+						temp.append(m)
+					if temp[-1] == '':
+						temp = temp[:-1]
+					if len(temp) != 0:
+						activesite.append(temp)
+			if message:
+				print "Coordinates successfully obtained!"
+			else:
+				print "Unable to open and extract active site coordinates from provided file. Please ensure that the file containes 1 set of 3D coordinates in the first three columns of the sheet in the form 'X Y Z'"
+				return
 
 	#Cleaning pdb of all extraneous information and renumbering
 
@@ -90,10 +164,96 @@ def enhance_enzyme_solubility():
 		return
 	except subprocess.CalledProcessError as f:
 		print f.output
-		return 
-	else:
-		print "PDB successfully renumbered!"
+		return
 
+	cleaned_pdb = pdb[:len(pdb)-4]+'_'+chain+'.pdb'
+
+	#Extracting Calpha backbone coordinates from cleaned pdb
+
+	print "Extracting Calpha backbone..."
+
+	with open(cleaned_pdb) as trunc_file:
+		for line in trunc_file:
+			items = line.split()
+			if items[0] == 'ATOM':
+				if (items[2] == 'CA'):
+					position = [items[5]]
+					position.append(items[3])
+					position.extend(items[6:9])
+					backbone.append(position)
+
+	#Using the Average Degree Filter to calculate the Contact Number for each residue
+
+	contact_nums = contact_number(cleaned_pdb,options.rpath, oscompiler, backbone)
+
+	#Calculate the minimum distance to the active site for each residue
+
+	dist_asite = distance_to_active_site(backbone,activesite)
+
+	#Concatenate results to one list
+
+	residue = 0
+	match = True
+	for res in dist_asite:
+		temp = []
+		current_contact_num_id = contact_nums[residue][0]
+		current_contact_num = contact_nums[residue][1]
+		if (res[1] == current_contact_num_id):
+			temp.append(res[0])
+			temp.append(res[1])
+			temp.append(res[2])
+			info.append(temp)
+	if match == False:
+		print "For missing contact numbers, refer to the file entitled 'contact_numbers.txt' generated in the current directory."
+
+	#Write resulting list to a CSV file
+
+	if len(info) > 4:
+		print "Generating CSV file with results..."
+		with open(options.outfilename, 'w') as newfile:
+			write = csv.wrtier(newfile,quoting=csv.QUOTE_ALL)
+			write.writerows(info)
+		print "File generated and placed in current directory! Name: "+"'"+options.outfilename+"'"
+	else:
+		print "Unable to calculate the distance to active site. Please check if the columns in the input file are separated by spaces and no other punctuation."
+	with open(options.outfilename) as thefile:
+		if sum(1 for line in thefile) == 0:
+			print "Unable to write results to a csv file. Printing resulting list instead."
+			print info
+
+	try:
+		os.remove(cleaned_pdb)
+		os.remove(cleaned_pdb[:cleaned_pdb.find('.')]+'.fasta')
+		os.remove(cleaned_pdb[:len(cleaned_pdb)-4]+"_0001.pdb")
+	except OSError as e:
+		print e, "Can't locate cleaned file that was generated. Aborting..."
+		return
+
+	return options.outfilename
+
+def verify_line(line):
+	#searches for erros in formatting the pdb file
+	value = 0
+	length = len(line)
+	if length < 12:
+		if len(line[2]) > 3:
+			value = 2
+		elif len(line[3]) > 4:
+			value += 1
+		elif len(line[4]) > 4:
+			value = 4
+		elif len(line[9]) > 4:
+			value = 0
+		else:
+			value = -1
+		return value
+	else:
+		return value
+
+def contact_number(cleaned_pdb, rpath,oscompiler,backbone):
+	print "Calculating the contact number for each residue..."
+	sub_call = rpath+'main/source/bin/rosetta_scripts.'+oscompiler+'release -database '+rpath+'main/database/'+' -parser:protocol '+'../contact_num.xml -s '+cleaned_pdb+' -ignore_unrecognized_res -ex1 -ex2 -use_input_sc -no_his_his'
+def euclidean(list1, list2)
 	
 def main():
 	if options.path == "GenerateMutations":
