4513c6a04b55ec8cbf1b9aa4a4ec8fb0affefc55
Rachel Clune <rachel.clune@omsf.io>
2025-11-04 16:28:06 -0600

    Added in printing the average score during the monte carlo procedure.

--- pilot/apps/rclune/bootcamp_app.py
+++ pilot/apps/rclune/bootcamp_app.py
@@ -22,7 +22,7 @@ print(f"Original score: {myscore}")
 # 5. Create Monte Carlo Protocol to Optimize Pose
 
 # C. Initialize a MonteCarloObject and loop over residues
-mymc = rosetta.protocols.moves.MonteCarlo(mypose, sfxn, 100)
+mymc = rosetta.protocols.moves.MonteCarlo(mypose, sfxn, 1.0)
 
 # 7. MoveMap stuff 
 mm = rosetta.core.kinematics.MoveMap()
@@ -45,13 +45,17 @@ minimizer.run(mypose, mm, sfxn, min_opts)
 the_observer = rosetta.protocols.moves.PyMOLObserver()
 the_observer.pymol().apply(mypose)
 
-for ii in range(100):
+acceptance_rate = 0
+cumulative_acceptance_rate = 0
+print_rate = 100
+
+for ii in range(1000):
+    print(f"Step {ii}: ")
 
     # A. get your random residue and random perturbations
     myres = np.random.randint(1, mypose.total_residue()+1)
     my_phi = np.random.normal()
     my_psi = np.random.normal()
-    print(sfxn(mypose))
 
     # B. perturb the pose, paying attention to if the residue is not an amino acid
     try:
@@ -67,8 +71,21 @@ for ii in range(100):
     rosetta.core.pack.pack_rotamers(mypose, sfxn, task)
     minimizer.run(mypose, mm, sfxn, min_opts)
 
-    mymc.boltzmann(mypose)
+    is_accepted = mymc.boltzmann(mypose)
+    print(is_accepted)
+    if is_accepted:
+        acceptance_rate += 1
+        cumulative_acceptance_rate += 1
+    
     the_observer.pymol().apply(mypose)
+
+    if ii % print_rate == 0 and ii != 0:
+        print(f"Acceptance rate: {acceptance_rate/print_rate}")
+        acceptance_rate = 0 # reset acceptance rate to 0
+        print(f"Cumulative acceptance rate: {cumulative_acceptance_rate/ii}")
+        print(mymc.show_counters())
+        print(f"Average score: {mypose.energies().total_energy()}")
+
 
 
 print(mymc.last_accept())
